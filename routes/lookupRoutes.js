4bdd391838ed15647718cda72a313b97b56ff255
shimyon <shimyon@hotmail.com>
2023-04-19 11:33:12 +0530

    Add group by on lookup

--- routes/lookupRoutes.js
+++ routes/lookupRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router();
 
-const { addLookup ,getAllLookupData,editLookup,getLookupById} = require('../controllers/lookupController')
+const { addLookup ,getAllLookupData,editLookup,getLookupById, getLookupByGroup} = require('../controllers/lookupController')
 
 const { protect } = require('../middleware/authMiddleware')
 router.post('/save', addLookup);
 router.get('/getAllLookup', protect, getAllLookupData)
 router.post('/editLookup', protect, editLookup)
 router.get('/getLookupById/:id', protect, getLookupById)
+router.get('/getLookupByGroup', protect, getLookupByGroup)
 module.exports = router
