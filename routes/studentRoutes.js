60cd4692f5995317ef0205168494cb181965e586
shimyon <shimyon@hotmail.com>
2023-05-24 17:15:43 +0530

    Mobile and Email verification uploaded

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -34,7 +34,7 @@ router.post('/deleteEducation', protect, deleteEducation)
 router.get('/getStudentEducation/:studentid', protect, getStudentEducation)
 router.get('/getStudentworkExperience/:studentid', protect, getStudentworkExperience)
 router.get('/getStudentlanguages/:studentid', protect, getStudentlanguages)
-router.post('/MobileVerify', protect, MobileVerify)
-router.post('/EmailVerify', protect, EmailVerify)
+router.post('/MobileVerify', MobileVerify)
+router.post('/EmailVerify', EmailVerify)
 
 module.exports = router
